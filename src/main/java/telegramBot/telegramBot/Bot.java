42e2b3acaf80248988f454cfbca79f349ce9393c
Denys_Moiseienko <denys.moiseienko1@gmail.com>
2021-08-04 12:37:39 +0200

    add simple bot

---
+++ src/main/java/telegramBot/telegramBot/Bot.java
@@ -0,0 +1,47 @@
+package telegramBot.telegramBot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${telegram.bot.username}")
+    private String botUsername;
+
+    @Value("${telegram.bot.token}")
+    private String botToken;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                String text = message.getText();
+                SendMessage sm = new SendMessage();
+                sm.setText("You said: " + text);
+                sm.setChatId(String.valueOf(message.getChatId()));
+                try {
+                    execute(sm);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/telegramBot/telegramBot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package telegramBot.telegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
