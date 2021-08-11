5c52b62619911ce8c409f849b7e5d1687f990bde
Denys_Moiseienko <denys.moiseienko1@gmail.com>
2021-08-11 13:32:48 +0200

    add messageSource

--- src/main/java/telegramBot/telegramBot/services/SendMessageService.java
+++ src/main/java/telegramBot/telegramBot/services/SendMessageService.java
@@ -1,5 +1,7 @@
 package telegramBot.telegramBot.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -11,16 +13,29 @@ public class SendMessageService {
     private final MessageSender messageSender;
     private final WeatherAPI weatherAPI;
 
+    private MessageSource messageSource;
+
     public SendMessageService(MessageSender messageSender, WeatherAPI weatherAPI) {
         this.messageSender = messageSender;
         this.weatherAPI = weatherAPI;
     }
 
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     public void send(Message message) {
+        String result = "/start".equals(message.getText())
+                ? messageSource.getMessage("greeting", null, null)
+                : weatherAPI.getWeatherInfo(message.getText());
+
         SendMessage sm = SendMessage.builder()
-                .text(weatherAPI.getWeatherInfo(message.getText()))
+                .text(result)
+                .parseMode("HTML")
                 .chatId(String.valueOf(message.getChatId()))
                 .build();
+
         messageSender.sendMessage(sm);
     }
 }
--- src/main/java/telegramBot/telegramBot/services/WeatherAPIService.java
+++ src/main/java/telegramBot/telegramBot/services/WeatherAPIService.java
@@ -1,11 +1,11 @@
 package telegramBot.telegramBot.services;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import telegramBot.telegramBot.weatherElements.Result;
-import telegramBot.telegramBot.weatherElements.Weather;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
@@ -16,23 +16,26 @@ public class WeatherAPIService implements WeatherAPI{
     @Value("${open.weather.map.id}")
     private String appId;
 
+    private MessageSource messageSource;
+
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @Override
     public Result getResponse(String message) throws IOException {
         URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + message +
                 "&units=metric&appid=" + appId);
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(url, Result.class);
+        return new ObjectMapper().readValue(url, Result.class);
     }
 
     @Override
     public String getWeatherInfo(String message) {
-        if (message.equals("/start")) {
-            return "Hi, I am a Simple Bot, send me title of city and I will say you about the weather:)";
-        }
         try {
             return createString(getResponse(message));
         }catch (FileNotFoundException e) {
-            return "Sorry, but I not found this city:(";
+            return messageSource.getMessage("notFound", null, null);
         } catch (IOException e) {
             return e.getMessage();
         }
@@ -40,13 +43,15 @@ public class WeatherAPIService implements WeatherAPI{
 
     private String createString(Result result) {
         String main = result.getName();
-        String temp = result.getMain().getTemp() + "C";
-        String description = "";
-        for (Weather w : result.getWeather()) {
-            description = w.getDescription();
-        }
-        return "City: " + main + "\n" +
-                "Temperature: " + temp + "\n" +
-                "Description: " + description;
+        double temp = result.getMain().getTemp();
+        String description = result.getWeather().stream().findFirst().get().getDescription();
+        int pressure = result.getMain().getPressure();
+        int humidity = result.getMain().getHumidity();
+
+        return "<i>City: </i><b>" + main + "</b>\n" +
+                "<i>Temperature: </i><b>" + temp + " C</b>\n" +
+                "<i>Description: </i><b>" + description + "</b>\n" +
+                "<i>Pressure: </i><b>" + pressure + " Pa</b>\n" +
+                "<i>Humidity: </i><b>" + humidity + "%</b>";
     }
 }
