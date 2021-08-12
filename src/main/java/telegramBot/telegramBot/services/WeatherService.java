75210dcf82aea9273471286058882b9406b7686a
Denys_Moiseienko <denys.moiseienko1@gmail.com>
2021-08-12 12:32:50 +0200

    add method getTime(), add new interface SendMessageService, change titles of classes

--- src/main/java/telegramBot/telegramBot/services/WeatherService.java
+++ src/main/java/telegramBot/telegramBot/services/WeatherService.java
@@ -1,10 +1,9 @@
 package telegramBot.telegramBot.services;
 
 import telegramBot.telegramBot.weatherElements.Result;
-
 import java.io.IOException;
 
-public interface WeatherAPI {
+public interface WeatherService {
 
     Result getResponse(String message) throws IOException;
     String getWeatherInfo(String message);
--- src/main/java/telegramBot/telegramBot/services/WeatherServiceImpl.java
+++ src/main/java/telegramBot/telegramBot/services/WeatherServiceImpl.java
@@ -9,9 +9,13 @@ import telegramBot.telegramBot.weatherElements.Result;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Service
-public class WeatherAPIService implements WeatherAPI{
+public class WeatherServiceImpl implements WeatherService {
 
     @Value("${open.weather.map.id}")
     private String appId;
@@ -47,11 +51,21 @@ public class WeatherAPIService implements WeatherAPI{
         String description = result.getWeather().stream().findFirst().get().getDescription();
         int pressure = result.getMain().getPressure();
         int humidity = result.getMain().getHumidity();
+        String sunrise = getTime(Long.sum(result.getSys().getSunrise(), result.getTimezone()));
+        String sunset = getTime(Long.sum(result.getSys().getSunset(), result.getTimezone()));
 
         return "<i>City: </i><b>" + main + "</b>\n" +
                 "<i>Temperature: </i><b>" + temp + " C</b>\n" +
                 "<i>Description: </i><b>" + description + "</b>\n" +
                 "<i>Pressure: </i><b>" + pressure + " Pa</b>\n" +
-                "<i>Humidity: </i><b>" + humidity + "%</b>";
+                "<i>Humidity: </i><b>" + humidity + "%</b>\n" +
+                "<i>Sunrise: </i><b>" + sunrise + "</b>\n" +
+                "<i>Sunset: </i><b>" + sunset + "</b>";
+    }
+
+    private String getTime(long time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm", Locale.ENGLISH)
+                .withZone(ZoneId.of("UTC"));
+        return formatter.format(Instant.ofEpochSecond(time));
     }
 }
